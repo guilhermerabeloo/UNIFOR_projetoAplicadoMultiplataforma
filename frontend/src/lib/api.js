defe93abb1402072ff8ab6f4722f8d799a60f897
Guilherme Rabelo <guilhermerabelo699@gmail.com>
2025-11-15 10:59:24 -0300

    Feat/Altera url base do backend, na configuracao do frontend, para apontar para o vercel

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 // Configura o objeto axios que sera a ponte entre o frontend e o backend
 export const api = axios.create({
-    baseURL: 'http://localhost:3000'
+    baseURL: 'https://feira-flow-backend.vercel.app/'
 })
